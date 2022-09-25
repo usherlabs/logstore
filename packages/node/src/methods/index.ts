efe7b79bfdab19836a7481365b05880ae32f2d26
Ryan Soury <ryan@webdoodle.com.au>
2022-09-26 03:30:13 +1000

    remove method

--- packages/node/src/methods/index.ts
+++ packages/node/src/methods/index.ts
@@ -2,6 +2,5 @@ export * from './approveTransactions';
 export * from './createTransactions';
 export * from './voteTransactions';
 export * from './runListener';
-export * from './resetListener';
 export * from './proposeBundle';
 export * from './validateBundleProposal';
