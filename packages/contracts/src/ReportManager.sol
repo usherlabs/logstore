a184ac1f6cb62b974257dfc47c27376afbf88381
Ryan Soury <ryan@webdoodle.com.au>
2023-03-23 23:17:07 +1100

    add comment to reporter list creation

--- packages/contracts/src/ReportManager.sol
+++ packages/contracts/src/ReportManager.sol
@@ -105,6 +105,7 @@ contract LogStoreReportManager is Initializable, UUPSUpgradeable, OwnableUpgrade
             address nextReporterAddress = address(0);
             for (uint256 j = 0; j < nodeAddresses.length; j++) {
                 if (reputationOf[nodeAddresses[j]] >= ceilingReputation && ceilingReputation > 0) {
+                    // Skip the reporter address that already been added to the reporters list
                     continue;
                 }
                 if (nextReporterAddress == address(0)) {
