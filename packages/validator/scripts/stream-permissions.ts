3cbb59bcac4ac6f982566324144822c202bb5f8f
Ryan Soury <ryan@webdoodle.com.au>
2023-04-07 20:54:48 +1000

    using scripts to manage dev

---
+++ packages/validator/scripts/stream-permissions.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { CONFIG_TEST, StreamrClient } from 'streamr-client';
+
+const BROKER_NODE_PRIVATE_KEY =
+	'0xb1abdb742d3924a45b0a54f780f0f21b9d9283b231a0a0b35ce5e455fa5375e7' as const;
+const systemStreamId =
+	'0x55B183b2936B57CB7aF86ae0707373fA1AEc7328/system' as const;
+// Should be deployed here http://localhost/core/streams/0x55B183b2936B57CB7aF86ae0707373fA1AEc7328%2Fsystem
+
+const client = new StreamrClient({
+	...CONFIG_TEST,
+	logLevel: 'trace',
+	auth: {
+		privateKey: BROKER_NODE_PRIVATE_KEY,
+	},
+});
+
+(async () => {
+	const stream = await client.getStream(systemStreamId);
+	const p = await stream.getPermissions();
+	console.log('STREAM:', stream.id);
+	console.log('PERMISSIONS:', p);
+})();
