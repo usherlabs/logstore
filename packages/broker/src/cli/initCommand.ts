038f0e0b427c9dc7aed0117fdaa1ae48c8e1959f
xander <shuaibualexander@gmail.com>
2023-04-19 17:35:16 +0100

    Remove abbreviation from initialise node cli command

--- packages/broker/src/cli/initCommand.ts
+++ packages/broker/src/cli/initCommand.ts
@@ -3,7 +3,7 @@ import { Command } from 'commander';
 import { startConfigWizard } from '../config/ConfigWizard';
 
 export const initCommand = new Command('init')
-	.description('init the configuration for the broker node')
+	.description('Initialise the configuration for the broker node')
 	.action(async () => {
 		try {
 			startConfigWizard();
