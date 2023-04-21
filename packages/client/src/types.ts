bdf19d33ff30d6152a9d203012f487fd6f3cb73f
xander <shuaibualexander@gmail.com>
2023-04-21 18:30:45 +0100

    implement browser build script for client

--- packages/client/src/types.ts
+++ packages/client/src/types.ts
@@ -3,4 +3,12 @@
  */
 import { F } from 'ts-toolbelt';
 
+import { LogStoreClient } from './LogStoreClient';
+
 export type MaybeAsync<T extends F.Function> = T | F.Promisify<T>; // Utility Type: make a function maybe async
+
+declare global {
+	interface Window {
+		LogStoreClient: LogStoreClient;
+	}
+}
