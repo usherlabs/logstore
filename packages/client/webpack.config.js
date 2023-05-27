c96dd1594c50de29bf15361f1aa34e04477a1eac
Ryan Soury <ryan@webdoodle.com.au>
2023-05-27 18:30:20 +1000

    successfully building browser bundle with modules

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -87,54 +87,54 @@ module.exports = (_, argv) => {
 		},
 		resolve: {
 			alias: {
-				'@concertodao/streamr-client': require.resolve(
-					'@concertodao/streamr-client/src/exports-browser.ts'
+				'@streamr-client': require.resolve(
+					'./modules/streamr-client/src/exports-browser.ts'
 				),
 				stream: 'readable-stream',
 				util: 'util',
 				http: require.resolve(
-					'@concertodao/streamr-client/src/shim/http-https.ts'
+					'./modules/streamr-client/src/shim/http-https.ts'
 				),
 				'@ethersproject/wordlists': require.resolve(
 					'@ethersproject/wordlists/lib/browser-wordlists.js'
 				),
 				https: require.resolve(
-					'@concertodao/streamr-client/src/shim/http-https.ts'
+					'./modules/streamr-client/src/shim/http-https.ts'
 				),
 				crypto: require.resolve('crypto-browserify'),
 				buffer: require.resolve('buffer/'),
 				'node-fetch': require.resolve(
-					'@concertodao/streamr-client/src/shim/node-fetch.ts'
+					'./modules/streamr-client/src/shim/node-fetch.ts'
 				),
 				'@streamr/protocol': path.resolve(
-					'./streamr-network/packages/protocol/src/exports.ts'
+					'./modules/streamr-protocol/src/exports.ts'
 				),
 				'@streamr/network-node': path.resolve(
-					'./streamr-network/packages/network/src/exports-browser.ts'
+					'./modules/streamr-network-node/src/exports-browser.ts'
 				),
 				[path.join(
 					__dirname,
-					'./streamr-network/packages/network/src/connection/webrtc/NodeWebRtcConnection.ts$'
-				)]: require.resolve(
-					'@streamr/network-node/src/connection/webrtc/BrowserWebRtcConnection.ts'
+					'./modules/streamr-network-node/src/connection/webrtc/NodeWebRtcConnection.ts$'
+				)]: path.resolve(
+					'./modules/streamr-network-node/src/connection/webrtc/BrowserWebRtcConnection.ts'
 				),
 				[path.join(
 					__dirname,
-					'./streamr-network/packages/network/src/connection/ws/NodeClientWsEndpoint.ts$'
-				)]: require.resolve(
-					'@streamr/network-node/src/connection/ws/BrowserClientWsEndpoint.ts'
+					'./modules/streamr-network-node/src/connection/ws/NodeClientWsEndpoint.ts$'
+				)]: path.resolve(
+					'./modules/streamr-network-node/src/connection/ws/BrowserClientWsEndpoint.ts'
 				),
 				[path.join(
 					__dirname,
-					'./streamr-network/packages/network/src/connection/ws/NodeClientWsConnection.ts$'
-				)]: require.resolve(
-					'@streamr/network-node/src/connection/ws/BrowserClientWsConnection.ts'
+					'./modules/streamr-network-node/src/connection/ws/NodeClientWsConnection.ts$'
+				)]: path.resolve(
+					'./modules/streamr-network-node/src/connection/ws/BrowserClientWsConnection.ts'
 				),
 				// swap out ServerPersistence for BrowserPersistence
 				[path.resolve(
-					'./streamr-network/packages/client/src/utils/persistence/ServerPersistence.ts'
+					'./modules/streamr-client/src/utils/persistence/ServerPersistence.ts'
 				)]: path.resolve(
-					'./streamr-network/packages/client/src/utils/persistence/BrowserPersistence.ts'
+					'./modules/streamr-client/src/utils/persistence/BrowserPersistence.ts'
 				),
 			},
 			fallback: {
