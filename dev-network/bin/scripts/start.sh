f75a8f00435b238833db460839245b67160bfcda
Victor Shevtsov <victorshevtsov@gmail.com>
2023-06-03 00:16:21 +0000

    kyve and arweave setup in dev-network

--- dev-network/bin/scripts/start.sh
+++ dev-network/bin/scripts/start.sh
@@ -11,10 +11,10 @@ done
 
 if [ -n "$LOCAL" ];
 then
-  streamr-docker-dev start --wait
+  streamr-docker-dev start
 
   cd $DEV_NETWORK_DIR
-  docker compose up -d
+    docker compose up --build -d
 else
   source $DEV_NETWORK_SCRIPTS_DIR/config_load.sh
   $SSH dev-network start -l
