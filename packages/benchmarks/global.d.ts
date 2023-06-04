7a03860d65863b953e8d096b5fd8d38267f516e1
Ryan Soury <ryan@webdoodle.com.au>
2023-06-04 21:31:26 +1000

    preparing public facing cli commands that can be used to support internal processes like benchmarks testing

---
+++ packages/benchmarks/global.d.ts
@@ -0,0 +1,11 @@
+declare global {
+	let describe: any;
+	let benchmark: any;
+	let run: any;
+	let beforeAll: any;
+	let afterAll: any;
+	let before: any;
+	let after: any;
+}
+
+export {};
--- packages/benchmarks/src/client.benchmark.ts
+++ packages/benchmarks/src/client.benchmark.ts
@@ -1,3 +1,4 @@
+
 // import LogStoreClient, {
 // 	Stream,
 // 	StreamPermission,
