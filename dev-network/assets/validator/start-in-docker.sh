666d2b78898eabe48b6357e2811198fdcf616bbb
Victor Shevtsov <victorshevtsov@gmail.com>
2023-06-05 06:16:16 +0000

    call `cli query stake` before starting validators in dev-network

---
+++ dev-network/assets/validator/start-in-docker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup. Let the validator node join the logstore network --"
+    logstore-cli --host http://sidechain:8546 --wallet $EVM_PRIVATE_KEY query stake -y 1000000000000000000
+else
+    echo "-- Not first container startup. Starting the validator node --"
+    logstore-validator start \
+        --pool 0 \
+        --valaccount VALACCOUNT \
+        --storage-priv STORAGE_PRIV \
+        --chain-id kyve-local \
+        --rpc http://logstore-kyve:26657 \
+        --rest http://logstore-kyve:1317
+fi
