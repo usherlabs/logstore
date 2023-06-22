d8b18d1952a83e337bc223baac028478ebbb1a25
Raffael Campos <raffael.rcampos@gmail.com>
2023-06-22 00:10:22 -0300

    setup benchmarks configuration correctly

--- packages/benchmarks/vitest.config.ts
+++ packages/benchmarks/vitest.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
 		deps: {
-			inline: ["vitest-fixture"],
+			inline: ['vitest-fixture'],
 		},
+		// load env variables
+		setupFiles: 'dotenv/config',
+		include: ['**/?(*.){test,spec,benchmark}.?(c|m)[jt]s?(x)'],
+		includeSource: ['src/**/*.{js,ts}'],
+		testTimeout: 220_000,
+		dangerouslyIgnoreUnhandledErrors: true,
 	},
 });
