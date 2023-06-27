ce74aa665877d4724222a5e18619f63050046df4
Ryan Soury <ryan@webdoodle.com.au>
2023-06-28 01:46:23 +1000

    minor dev-network script update

--- dev-network/bin/scripts/deploy.sh
+++ dev-network/bin/scripts/deploy.sh
@@ -21,7 +21,7 @@ then
   cd "$DEV_NETWORK_DIR/.."
   git fetch
   git checkout $BRANCH
-  git pull
+  git pull || echo "\nContinuing..."
 
   echo Starting the DevNetwork...
   "$DEV_NETWORK_SCRIPTS_DIR/start.sh" -l
