d37a6fc634511121085bb11ce6e5f18f6a9a6e4c
Victor Shevtsov <victorshevtsov@gmail.com>
2023-06-28 13:03:17 +0000

    increase amounts of minted and staked tokens for DevNetwork

--- dev-network/assets/broker/start-in-docker.sh
+++ dev-network/assets/broker/start-in-docker.sh
@@ -4,7 +4,7 @@ CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
     touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup. Let the broker node join the logstore network --"
-    logstore-broker join 1000000000000000000 -y -m "$BROKER_METADATA"
+    logstore-broker join 1000000000000000000000000000000 -y -m "$BROKER_METADATA"
 else
     echo "-- Not first container startup. Starting the broker node --"
     logstore-broker start
