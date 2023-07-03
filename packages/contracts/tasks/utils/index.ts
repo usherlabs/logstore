41fc63537972443589e166e4a0de18a464811386
Ryan Soury <ryan@webdoodle.com.au>
2023-07-03 18:10:15 +1000

    add fetch for min-stake-amount

--- packages/contracts/tasks/utils/index.ts
+++ packages/contracts/tasks/utils/index.ts
@@ -4,20 +4,25 @@ import ContractAddresses from '../../address.json';
 
 export const getTaskConfig = async (hre: HardhatRuntimeEnvironment) => {
 	const { LSAN__factory } = await import('../..');
+	const { LogStoreNodeManager__factory } = await import('../..');
 	const chainId = hre.network.config.chainId;
 	console.log('Chain Id:', chainId);
 	const chainIdIndex = `${chainId}` as keyof typeof ContractAddresses;
 	const [signer] = await hre.ethers.getSigners();
-	const { tokenManagerAddress: lsanTokenAddress } = ContractAddresses[
-		chainIdIndex
-	] as any;
+	const { tokenManagerAddress: lsanTokenAddress, nodeManagerAddress } =
+		ContractAddresses[chainIdIndex] as any;
 	console.log('LSAN Token Address:', lsanTokenAddress);
 	const tokenContract = LSAN__factory.connect(lsanTokenAddress, signer);
+	const nodeManagerContract = LogStoreNodeManager__factory.connect(
+		nodeManagerAddress,
+		signer
+	);
 
 	return {
 		chainId,
 		signer,
 		LSAN__factory,
 		tokenContract,
+		nodeManagerContract,
 	};
 };
