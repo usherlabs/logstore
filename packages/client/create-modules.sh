26900ff8a9bd42a7ef21619db9d02964998c62cf
Ryan Soury <ryan@webdoodle.com.au>
2023-07-05 00:10:11 +1000

    second attempt here is to normalize the modules mechanism -- this makes the monorepo even more massive... which is not the best approach.

--- packages/client/create-modules.sh
+++ packages/client/create-modules.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-rm -rf modules .streamr-network
-git clone git@github.com:usherlabs/streamr-network.git ./.streamr-network
-cd ./.streamr-network
-git checkout logstore
-npm i
-cd ./packages/client
-npm i
-bash ./vendor-hack.sh
-cd ../../../
+if [ -d "$DIR" ];
+then
+	echo "Log Store: Streamr Client Module is loaded. Producing client package modules..."
+else
+	echo "$DIR does not exist. Please use 'pnpm modules' from root directory."
+	exit;
+fi
+
+rm -rf ./modules
 mkdir -p ./modules/streamr/client
 mkdir -p ./modules/streamr/network-node
 mkdir -p ./modules/streamr/protocol
@@ -19,4 +19,5 @@ cp -R ./.streamr-network/packages/client/vendor ./modules/streamr/client/vendor
 cp -R ./.streamr-network/packages/network/src ./modules/streamr/network-node/src
 # cp -R ./.streamr-network/packages/protocol/tsconfig.node.json ./modules/streamr/protocol/tsconfig.node.json
 cp -R ./.streamr-network/packages/protocol/src ./modules/streamr/protocol/src
-rm -rf .streamr-network
+
+echo "Log Store: Client package modules successfully created."
