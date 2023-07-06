ef75eaa80f0ce34ea4b3c29ef2bf555436cdcfb9
Victor Shevtsov <victorshevtsov@gmail.com>
2023-07-06 15:03:54 +0000

    fix validator's `withSources` method

--- packages/validator/src/managers/Managers.ts
+++ packages/validator/src/managers/Managers.ts
@@ -5,6 +5,7 @@ import {
 	getReportManagerContract,
 	getStoreManagerContract,
 } from '@logsn/shared';
+import { cloneDeep } from 'lodash';
 
 import { getEvmPrivateKey } from '../env-config';
 import { NodeManager } from './NodeManager';
@@ -61,12 +62,50 @@ export class Managers {
 			})
 		);
 
+		const clean = (value: object) => {
+			if (!value) return value;
+
+			if (Array.isArray(value)) {
+				for (const item of value) {
+					clean(item);
+				}
+			}
+
+			// if the object has "provider" property with "connection" property then delete the "provider"
+			if (typeof value === 'object') {
+				for (const key of Object.keys(value)) {
+					if (key === 'provider' && value[key].connection) {
+						delete value[key];
+					} else {
+						clean(value[key]);
+					}
+				}
+			}
+
+			return value;
+		};
+
 		// check if results from the different sources match
-		if (
-			!results.every((b) => JSON.stringify(b) === JSON.stringify(results[0]))
-		) {
-			throw new Error(`Sources returned different results`);
-		}
+		const a = results[0];
+		const srcA = sources[0];
+		const objA = clean(cloneDeep(a));
+		const strA = JSON.stringify(objA);
+
+		results.slice(1).forEach((b, i) => {
+			const srcB = sources[i + 1];
+			const objB = clean(cloneDeep(b));
+			const strB = JSON.stringify(objB);
+
+			if (strA !== strB) {
+				const diff = {
+					[srcA]: objA,
+					[srcB]: objB,
+				};
+				throw new Error(
+					`Sources returned different results: ${JSON.stringify(diff)}`
+				);
+			}
+		});
 
 		return results[0];
 	}
