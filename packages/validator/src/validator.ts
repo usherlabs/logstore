3bc35efc78f7e880183c3f60f166d617918e240a
Victor Shevtsov <victorshevtsov@gmail.com>
2023-07-11 07:43:55 +0000

    attempt to unfreeze validators at alpha network

--- packages/validator/src/validator.ts
+++ packages/validator/src/validator.ts
@@ -15,39 +15,40 @@ export async function validateDataAvailability(this: Validator): Promise<void> {
 	}
 
 	const listenerHasValidData = async () => {
-		const getCurrentKeyMs = async () => {
-			/* eslint-disable */
-			const nextKey = this.pool.data!.current_key
-				? await this.runtime.nextKey(this, this.pool.data!.current_key)
-				: this.pool.data!.start_key;
-			/* eslint-enable */
+		// const getCurrentKeyMs = async () => {
+		// 	/* eslint-disable */
+		// 	const nextKey = this.pool.data!.current_key
+		// 		? await this.runtime.nextKey(this, this.pool.data!.current_key)
+		// 		: this.pool.data!.start_key;
+		// 	/* eslint-enable */
 
-			return parseInt(nextKey, 10) * 1000;
-		};
+		// 	return parseInt(nextKey, 10) * 1000;
+		// };
 
-		let currentKeyMs = await getCurrentKeyMs();
+		// let currentKeyMs = await getCurrentKeyMs();
 		while (
-			!this.runtime.listener.startTime ||
-			this.runtime.listener.startTime > currentKeyMs
+			!this.runtime.listener.startTime // ||
+			// this.runtime.listener.startTime > currentKeyMs
 		) {
 			if (!this.runtime.listener.startTime) {
 				this.logger.info(
 					'SystemListener is not started yet. Sleeping for 10 seconds...'
 				);
 				await sleep(10 * 1000);
-			} else {
-				const sleepMs = this.runtime.listener.startTime - currentKeyMs + 1000;
-				this.logger.info(
-					`SystemListener.startTime (${
-						this.runtime.listener.startTime
-					}) is greater than currentKeyMs (${currentKeyMs}). Sleeping for ${(
-						sleepMs / 1000
-					).toFixed(2)} seconds...`
-				);
-				await sleep(sleepMs);
 			}
-			await this.syncPoolState();
-			currentKeyMs = await getCurrentKeyMs();
+			// } else {
+			// 	const sleepMs = this.runtime.listener.startTime - currentKeyMs + 1000;
+			// 	this.logger.info(
+			// 		`SystemListener.startTime (${
+			// 			this.runtime.listener.startTime
+			// 		}) is greater than currentKeyMs (${currentKeyMs}). Sleeping for ${(
+			// 			sleepMs / 1000
+			// 		).toFixed(2)} seconds...`
+			// 	);
+			// 	await sleep(sleepMs);
+			// }
+			// await this.syncPoolState();
+			// currentKeyMs = await getCurrentKeyMs();
 		}
 	};
 
