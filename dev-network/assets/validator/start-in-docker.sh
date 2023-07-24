5296e24d8aab43a26f7c11e9f30ba6134a086b6b
Victor Shevtsov <victorshevtsov@gmail.com>
2023-07-24 19:57:55 +0000

    fix start-in-docker.sh scripts for broker and validator

--- dev-network/assets/validator/start-in-docker.sh
+++ dev-network/assets/validator/start-in-docker.sh
@@ -2,10 +2,10 @@
 
 CONTAINER_ALREADY_STARTED="/firstrun/CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-    touch $CONTAINER_ALREADY_STARTED
     echo "-- First container startup. Let the validator node join the logstore network --"
-    logstore --host http://sidechain:8546 --wallet $EVM_PRIVATE_KEY query stake -y 1000000000000000000000000000000
-    node ./cli/index.js
+    logstore --host http://sidechain:8546 --wallet $EVM_PRIVATE_KEY query stake -y 1000000000000000000000000000000 &&
+    node ./cli/index.js &&
+    touch $CONTAINER_ALREADY_STARTED
 else
     echo "-- Not first container startup. Starting the validator node --"
     logstore-validator start \
