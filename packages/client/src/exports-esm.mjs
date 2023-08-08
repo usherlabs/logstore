45e5cb81ec1ab4971900b7121d4221bee113d492
Raffael Campos <raffael.rcampos@gmail.com>
2023-08-08 12:47:19 -0300

    Add CONFIG_TEST to client ESM exports
    The client's ECMAScript module (ESM) exports have been updated to include CONFIG_TEST from the ConfigTest.js file. Without it, this configuration was previously being the one from streamr itself on some ESM environments

--- packages/client/src/exports-esm.mjs
+++ packages/client/src/exports-esm.mjs
@@ -1,6 +1,7 @@
 // ESM EntryPoint
 import LogStoreClient from './index.js';
 export * from './index.js';
+export { CONFIG_TEST } from './ConfigTest.js';
 // required to get import LogStoreClient from '@logsn/streamr-client' to work
 export default LogStoreClient.default;
 // note this file is manually copied as-is into dist/src since we don't want tsc to compile it to commonjs
