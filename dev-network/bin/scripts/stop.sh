562a1d0d489a0681a512a9b3b892cbeca269f99a
Raffael Campos <raffael.rcampos@gmail.com>
2023-10-23 08:22:49 -0300

    Enhance stop.sh script in dev-network
    This update refines the stop.sh script within the dev-network to handle docker component stopping more reliably. Rather than relying on a hardcoded path for the docker-compose.yml file, it now discovers the exact path dynamically. This ensures better compatibility across different systems and environments.

--- dev-network/bin/scripts/stop.sh
+++ dev-network/bin/scripts/stop.sh
@@ -15,7 +15,14 @@ then
 
   docker compose down -v
   streamr-docker-dev stop
-  docker compose -f ~/streamr-docker-dev/docker-compose.yml down -v
+
+  STREAMR_DOCKER_BIN=$(which streamr-docker-dev) # eg: /usr/local/bin/streamr-docker-dev
+  STREAMR_DOCKER_BIN_REAL=$(readlink $STREAMR_DOCKER_BIN -f) # eg: /home/user/streamr-docker-dev/streamr-docker-dev/bin/streamr-docker-dev
+  STREAMR_DOCKER_DIR=$(dirname $STREAMR_DOCKER_BIN_REAL/..) # eg: /home/user/streamr-docker-dev/streamr-docker-dev/..
+  STREAMR_DOCKER_DIR=$(readlink $STREAMR_DOCKER_DIR -f) # eg: /home/user/streamr-docker-dev
+
+
+  docker compose -f $STREAMR_DOCKER_DIR/docker-compose.yml down -v
 else
   source $DEV_NETWORK_SCRIPTS_DIR/config_load.sh
   $SSH dev-network stop -l
