f9a1e375f3422abf344c47337c78cebed40425d6
Victor Shevtsov <victorshevtsov@gmail.com>
2023-11-02 10:04:49 +0000

    Get rid of temporary streams: system, recovery, heartbeat

--- packages/broker/src/broker.ts
+++ packages/broker/src/broker.ts
@@ -45,21 +45,9 @@ export const createBroker = async (
 		config.client.contracts!.logStoreNodeManagerChainAddress!
 	);
 
-	const isDevNetwork =
-		nodeManagerAddress ===
-		toEthereumAddress('0x85ac4C8E780eae81Dd538053D596E382495f7Db9');
-
-	const heartbeatStreamId = isDevNetwork
-		? toStreamID('/heartbeat', nodeManagerAddress)
-		: '0xa156eda7dcd689ac725ce9595d4505bf28256454/alpha-heartbeat';
-
-	const recoveryStreamId = isDevNetwork
-		? toStreamID('/recovery', nodeManagerAddress)
-		: '0xa156eda7dcd689ac725ce9595d4505bf28256454/alpha-recovery';
-
-	const systemStreamId = isDevNetwork
-		? toStreamID('/system', nodeManagerAddress)
-		: '0xa156eda7dcd689ac725ce9595d4505bf28256454/alpha-system';
+	const heartbeatStreamId = toStreamID('/heartbeat', nodeManagerAddress);
+	const recoveryStreamId = toStreamID('/recovery', nodeManagerAddress);
+	const systemStreamId = toStreamID('/system', nodeManagerAddress);
 
 	const heartbeatStream = await logStoreClient.getStream(heartbeatStreamId);
 	const recoveryStream = await logStoreClient.getStream(recoveryStreamId);
