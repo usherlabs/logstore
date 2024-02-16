059d6784e6cf1c6b54879028a729f009c315f054
Raffael Campos <raffael.rcampos@gmail.com>
2024-02-16 09:15:15 -0300

    Add error handling for BroadbandObservable
    Implemented error handling for BroadbandObservable in the client utility functions. If the subscription fails with the error stating "Client is destroyed", the observer is completed, else, the occurred error is thrown. This approach simplifies the process as making new promises cancelable wouldn't be fully guaranteeing.

--- packages/client/src/utils/rxjs/BroadbandObservable.ts
+++ packages/client/src/utils/rxjs/BroadbandObservable.ts
@@ -21,6 +21,15 @@ export const createBroadbandObservable = (
 		// Notifies the observer about new messages, as they arrive to the subscriber
 		subscriber.subscribe((content, metadata) => {
 			observer.next([content, metadata]);
+		}).catch((e) => {
+			// If the subscription fails with the error containing "Client is destroyed" we called destroy already, then complete.
+			// Otherwise, we throw the error.
+			// Handling it another way is harder as we should make all new promises canceable, and it wouldn't guarantee.
+			if (e.message.includes('Client is destroyed')) {
+				observer.complete();
+			} else {
+				observer.error(e);
+			}
 		});
 
 		// Triggers BroadbandSubscriber#unsubscribe when the observable is unsubscribed
