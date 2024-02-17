2cba6e736051565983ac6fa5252d74fc5a2afb1c
Victor Shevtsov <victorshevtsov@gmail.com>
2024-02-17 00:35:39 +0000

    Use specific image streamr/broker-node:dev by its digest for the dev-network

--- dev-network/bin/scripts/start.sh
+++ dev-network/bin/scripts/start.sh
@@ -11,6 +11,11 @@ done
 
 if [ -n "$LOCAL" ];
 then
+  # Pull a very specific image streamr/broker-node:dev by its digest.
+  # That have to be removed when we update to be compatible with the original streamr dev network.
+  docker pull streamr/broker-node@sha256:6f55db912e10d4ee6b8d1c82ab4101b0671c5e2714ed4bd90d03558fe156a904
+  docker tag streamr/broker-node@sha256:6f55db912e10d4ee6b8d1c82ab4101b0671c5e2714ed4bd90d03558fe156a904 streamr/broker-node:dev
+
   streamr-docker-dev start
 
   cd $DEV_NETWORK_DIR
